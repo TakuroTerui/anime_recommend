744e927c811ca25e816cec1c088bc3a7ed3408ba
照井拓朗 <>
2022-04-22 22:00:42 +0900

    軽量化

--- animeproject/settings.py
+++ animeproject/settings.py
@@ -136,5 +136,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
